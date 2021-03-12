5287e279c28f2d5d3e55bc768233f1b304d4f7a9
FelixHerrera <viperedge94@yahoo.com>
2021-03-11 21:48:56 -0600

    Fixing material ui with next. Changed file structure. Added appbar to organizations page.

---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <MuiThemeProvider>
+        <Component {...pageProps} />
+      </MuiThemeProvider>
+    </>
+  );
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
---
+++ pages/organizations.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ButtonAppBar from '../src/components/Header/Header';
+
+const organizations = () => (
+  <ButtonAppBar name="Organizations" />
+);
+
+export default organizations;
