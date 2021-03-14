00df901b3c8051c6571713700cb155295e38b10e
FelixHerrera <viperedge94@yahoo.com>
2021-03-13 18:42:32 -0600

    removing unused code

--- src/components/Dialogs/ClientDialog.js
+++ src/components/Dialogs/ClientDialog.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
-import PersonIcon from '@material-ui/icons/Person';
-import AddIcon from '@material-ui/icons/Add';
 import Typography from '@material-ui/core/Typography';
-import { blue } from '@material-ui/core/colors';
-import Grid from '@material-ui/core/Grid';
 import MemberList from './MemberList/MemberList';
 
 import swrFetch from '../../common/swrFetch';
 
-const useStyles = makeStyles((theme) => ({
-  gridContainer: {
-    marginLeft: theme.spacing(2),
-  },
-}));
-
 const ClientDialog = ({ id, open, onClose }) => {
-  const classes = useStyles();
   const { data, error } = swrFetch(`https://5fe220547a9487001768215e.mockapi.io/api/v1/organization/${id}`);
 
   let content = <LinearProgress />;
